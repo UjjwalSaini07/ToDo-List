b988e994ac1a8efb67d3ed3194ac89f9b6039532
Ujjwal Saini <ujjwalsaini1947@gmail.com>
2024-07-31 17:42:53 +0530

    Add Theme Toggle
    Co-Authored-By: pavits31943 <145360907+pavits31943@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,35 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Home from './pages/home';
+import Remainder from './pages/remainder';
+import ThemeToggle from './pages/ThemeToggle';
 import './App.css';
 
 function App() {
-   const [darkMode, setDarkMode] = useState(() => {
-    const savedMode = localStorage.getItem('darkMode');
-    return savedMode ? JSON.parse(savedMode) : false;
-  });
-
-  useEffect(() => {
-    localStorage.setItem('darkMode', JSON.stringify(darkMode));
-    document.body.classList.toggle('dark-mode', darkMode);
-  }, [darkMode]);
-
-  const handleToggleDarkMode = () => setDarkMode(prevMode => !prevMode);
-
   return (
-    <div className={`App ${darkMode ? 'dark-mode' : 'light-mode'}`}>
-      <header className="App-header">
-        <button
-          className="mode-toggle-button"
-          onClick={handleToggleDarkMode}
-        >
-          {darkMode ? 'Light Mode' : 'Dark Mode'}
-        </button>
-      </header>
-
-      <main>
-        <Home />
-      </main>
-    </div>
+    <main>
+      <ThemeToggle />
+      <Home />
+      <Remainder />
+    </main>
   );
 }
 
